63f9ef715586c068512def7b13674ef9462cf52d
dhinogz <davidhinojosauni@gmail.com>
2022-02-07 02:22:28 -0600

    Add login_requiered decorators to book views

--- books/views.py
+++ books/views.py
@@ -1,12 +1,15 @@
 from django.template.response import TemplateResponse
+from django.contrib.auth.decorators import login_required
 from .models import Book
 
+@login_required
 def book_list_view(request):
     context ={}
     context["book_list"] = Book.objects.all()
 
     return TemplateResponse(request, "books/book_list.html", context)
 
+@login_required
 def book_detail_view(request, pk):
     context = {}
     context["book"] = Book.objects.get(pk=pk)
